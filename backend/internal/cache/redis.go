7c7855dff65ca63f26a9f1461c68b2936d8d86e1
hashinnadin <hashinnadin@gmail.com>
2026-04-29 13:29:23 +0530

    fix: backend folder issue

---
+++ backend/internal/cache/redis.go
@@ -0,0 +1,23 @@
+package cache
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+// Creating a single-noded stand alone redis
+var Ctx = context.Background()
+
+type Redis struct {
+	Client *redis.Client
+}
+
+func NewRedis() *Redis {
+	client := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+		DB:   0,
+	})
+
+	return &Redis{Client: client}
+}
